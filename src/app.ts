591567baad1a950a1deb9b46e045fda4455b665a
Philip Lewis <philipjonlewis@gmail.com>
2022-03-15 20:38:11 +0800

    solved mysql issues - must be mysql 2 and must have socketPath

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,33 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, {
+  Application,
+  NextFunction,
+  Request,
+  Response,
+  RequestHandler,
+} from "express";
+
 import bodyParser from "body-parser";
 
-const app:Application = express();
+const app: Application = express();
 
-app.get("/", (req:Request, res:Response, next:NextFunction) => {
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "07131992",
+  socketPath: "/tmp/mysql.sock",
+});
+
+connection.connect((err: any) => {
+  if (err) throw err;
+  console.log("Connected to MySQL Server!");
+});
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("hjel;l0");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("hello");
+});
